7af45e13a9ce88c7e2277a020e52a1a01478c77d
Arnaud Babol <arnaud@babol.me>
2017-11-22 13:34:55 +0100

    Rename icon too long

--- NavigationExperimentalComponents/NavigationHeaderBackButton.js
+++ NavigationExperimentalComponents/NavigationHeaderBackButton.js
@@ -21,24 +21,24 @@
  * @providesModule NavigationHeaderBackButton
  * @flow
 */
-"use strict";
+'use strict';
 
-const React = require("react");
-const ReactNative = require("react-native");
-const PropTypes = require("prop-types");
+const React = require('react');
+const ReactNative = require('react-native');
+const PropTypes = require('prop-types');
 
 const {
   I18nManager,
   Image,
   Platform,
   StyleSheet,
-  TouchableOpacity
+  TouchableOpacity,
 } = ReactNative;
 
 type Props = {
   imageStyle?: any,
   onPress: Function,
-  style?: any
+  style?: any,
 };
 
 const NavigationHeaderBackButton = (props: Props) => (
@@ -48,29 +48,29 @@ const NavigationHeaderBackButton = (props: Props) => (
   >
     <Image
       style={[styles.button, props.imageStyle]}
-      source={require("./assets/back-icon.png")}
+      source={require('./assets/b.png')}
     />
   </TouchableOpacity>
 );
 
 NavigationHeaderBackButton.propTypes = {
-  onPress: PropTypes.func.isRequired
+  onPress: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center"
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     height: 24,
     width: 24,
-    margin: Platform.OS === "ios" ? 10 : 16,
-    resizeMode: "contain",
-    transform: [{ scaleX: I18nManager.isRTL ? -1 : 1 }]
-  }
+    margin: Platform.OS === 'ios' ? 10 : 16,
+    resizeMode: 'contain',
+    transform: [{ scaleX: I18nManager.isRTL ? -1 : 1 }],
+  },
 });
 
 module.exports = NavigationHeaderBackButton;
